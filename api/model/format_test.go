c4d1d1336d00125113add75ad6adc1a799bee3ff
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-24 00:57:18 +0900

    validate by test code

---
+++ api/model/format_test.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+func TestFormat(t *testing.T) {
+	test := []struct {
+		name  string
+		str   string
+		match bool
+	}{
+		{
+			str: "stock.google.usa.1m",
+			match: true,
+		},
+		{
+			str: "stock.amazon.usa.1s",
+			match: true,
+		},
+		{
+			str: "test.goboolean.kor.r",
+			match: false,
+		},
+		{
+			str: "test.goboolean.kor.t",
+			match: true,
+		},
+		{
+			str: "stock.goboolean.kr.r",
+			match: false,
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			got := IsSymbolValid(tt.str)
+			assert.Equal(t, tt.match, got)
+		})
+	}
+}

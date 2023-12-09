1f1940c21cdcbcbea256f3e989b2c9da60b356b2
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-12-10 03:13:21 +0900

    create kafka admin

---
+++ internal/kafkaadmin/configurator.go
@@ -0,0 +1,174 @@
+package kafkaadmin
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/Goboolean/common/pkg/resolver"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+// Configurator has a role for making and deleting topic, checking topic exists, and getting topic list.
+type Configurator struct {
+	client *kafka.AdminClient
+
+	wg     sync.WaitGroup
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+var (
+	conf *Configurator
+	once sync.Once
+)
+
+func New(c *resolver.ConfigMap) (*Configurator, error) {
+
+	bootstrap_host, err := c.GetStringKey("BOOTSTRAP_HOST")
+	if err != nil {
+		return nil, err
+	}
+
+	config := &kafka.ConfigMap{
+		"bootstrap.servers": bootstrap_host,
+	}
+
+	conn, err := kafka.NewAdminClient(config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &Configurator{
+		client: conn,
+		ctx:    ctx,
+		cancel: cancel,
+	}, nil
+}
+
+func (c *Configurator) Close() {
+	c.cancel()
+	time.Sleep(time.Second * 1)
+	c.client.Close()
+	c.wg.Wait()
+}
+func (c *Configurator) Ping(ctx context.Context) error {
+
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return ErrDeadlineSettingRequired
+	}
+
+	_, err := c.client.GetMetadata(nil, true, int(time.Until(deadline).Milliseconds()))
+	return err
+}
+
+
+func (c *Configurator) CreateTopic(ctx context.Context, topic string) error {
+
+	exists, err := c.TopicExists(ctx, topic)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		return fmt.Errorf("topic already exists")
+	}
+
+	topicInfo := kafka.TopicSpecification{
+		Topic:             topic,
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}
+
+	result, err := c.client.CreateTopics(ctx, []kafka.TopicSpecification{topicInfo})
+	if err != nil {
+		return err
+	}
+
+	if err := result[0].Error; err.Code() != kafka.ErrNoError {
+		return fmt.Errorf(err.String())
+	}
+
+	return nil
+}
+
+
+func (c *Configurator) DeleteTopic(ctx context.Context, topic string) error {
+
+	result, err := c.client.DeleteTopics(ctx, []string{topic})
+
+	if err != nil {
+		return err
+	}
+
+	if err := result[0].Error; err.Code() != kafka.ErrNoError {
+		return err
+	}
+
+	return nil
+}
+
+
+func (c *Configurator) DeleteAllTopics(ctx context.Context) error {
+	topicList, err := c.GetTopicList(ctx)
+	if err != nil {
+		return err
+	}
+
+	result, err := c.client.DeleteTopics(ctx, topicList)
+	if err != nil {
+		return err
+	}
+
+	if err := result[0].Error; err.Code() != kafka.ErrNoError {
+		return fmt.Errorf(err.String())
+	}
+
+	return nil
+}
+
+
+func (c *Configurator) TopicExists(ctx context.Context, topic string) (bool, error) {
+
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return false, ErrDeadlineSettingRequired
+	}
+
+	metadata, err := c.client.GetMetadata(nil, true, int(time.Until(deadline).Milliseconds()))
+	if err != nil {
+		return false, err
+	}
+
+	_, exists := metadata.Topics[topic]
+	return exists, nil
+}
+
+
+func (c *Configurator) GetTopicList(ctx context.Context) ([]string, error) {
+
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return nil, ErrDeadlineSettingRequired
+	}
+
+	metadata, err := c.client.GetMetadata(nil, true, int(time.Until(deadline).Milliseconds()))
+	if err != nil {
+		return nil, err
+	}
+
+	topicList := make([]string, 0)
+
+	for topic := range metadata.Topics {
+		if len(topic) > 0 {
+			topicList = append(topicList, topic)
+		}
+	}
+
+	return topicList, nil
+}
---
+++ internal/kafkaadmin/configurator_test.go
@@ -0,0 +1,143 @@
+package kafkaadmin_test
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Goboolean/common/pkg/resolver"
+	"github.com/Goboolean/fetch-system.IaC/internal/kafkaadmin"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/Goboolean/common/pkg/env"
+)
+
+func SetupConfigurator() *kafkaadmin.Configurator {
+
+	conf, err := kafkaadmin.New(&resolver.ConfigMap{
+		"BOOTSTRAP_HOST": os.Getenv("KAFKA_BOOTSTRAP_HOST"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return conf
+}
+
+func TeardownConfigurator(conf *kafkaadmin.Configurator) {
+	conf.Close()
+}
+
+func Test_Configurator(t *testing.T) {
+
+	conf := SetupConfigurator()
+	defer TeardownConfigurator(conf)
+
+	t.Run("Ping", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		err := conf.Ping(ctx)
+		assert.NoError(t, err)
+	})
+}
+
+func Test_CreateDeleteTopic(t *testing.T) {
+
+	const topic = "test.goboolean.io.1s"
+
+	conf := SetupConfigurator()
+	defer TeardownConfigurator(conf)
+
+	t.Run("CreateTopic", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		err := conf.CreateTopic(ctx, topic)
+		assert.NoError(t, err)
+
+		exists, err := conf.TopicExists(ctx, topic)
+		assert.NoError(t, err)
+		assert.True(t, exists)
+	})
+
+	t.Run("CreateExitingTopic", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		err := conf.CreateTopic(ctx, topic)
+		assert.Error(t, err)
+	})
+
+	t.Run("DeleteTopic", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		err := conf.DeleteTopic(ctx, topic)
+		assert.NoError(t, err)
+
+		exists, err := conf.TopicExists(ctx, topic)
+		assert.NoError(t, err)
+		assert.False(t, exists)
+	})
+
+	t.Run("DeleteNonExistingTopic", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		err := conf.DeleteTopic(ctx, topic)
+		assert.Error(t, err)
+	})
+}
+
+
+func Test_GetTopicList(t *testing.T) {
+
+	conf := SetupConfigurator()
+	defer TeardownConfigurator(conf)
+
+	topicList := []string{"test.goboolean.io.1s", "test.goboolean.io.1t"}
+
+	t.Run("GenerateTopics", func(t *testing.T) {
+		
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		for _, topic := range topicList {
+			err := conf.CreateTopic(ctx, topic)
+			assert.NoError(t, err)
+		}
+	})
+
+	t.Run("GetTopicList", func(t *testing.T) {
+		
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		list, err := conf.GetTopicList(ctx)
+		assert.NoError(t, err)
+
+		assert.Len(t, list, len(topicList))
+		for _, topic := range topicList {
+			assert.Contains(t, list, topic)
+		}
+	})
+
+	t.Run("DeleteAllTopics", func(t *testing.T) {
+	
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+	
+		err := conf.DeleteAllTopics(ctx)
+		assert.NoError(t, err)
+
+		list, err := conf.GetTopicList(ctx)
+		assert.NoError(t, err)
+		assert.Len(t, list, 0)
+	})
+}

6cbd9d36fce9bb59957a6a20e186c32f8d587559
mulmuri <68294539+mulmuri@users.noreply.github.com>
2024-01-09 16:12:27 +0900

    commits of current works

--- internal/preparer/scenario_test.go
+++ internal/preparer/scenario_test.go
@@ -29,7 +29,7 @@ func TestScenario(t *testing.T) {
 	)
 
 	t.Run("Setup prerarer", func(tt *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		var err error

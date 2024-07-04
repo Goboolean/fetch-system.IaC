62c7cb89145bc04d180c79614f8e18cb37a3b73b
lsjtop10 <lsjtop10@gmail.com>
2024-07-04 10:14:28 +0900

    Feat: implement FetchLimitedTradeAfter query

--- pkg/influx/queries.go
+++ pkg/influx/queries.go
@@ -60,6 +60,65 @@ func (d *DB) FetchByTimeRange(
 	return data, nil
 }
 
+// FetchLimitedTradeAfter fetches limited trade data for a specific product and time frame after a given start time.
+// It queries the InfluxDB database to retrieve the stock aggregates (open, close, high, low, volume) for the specified product and time frame.
+// The number of results is limited by the 'limit' parameter.
+// The function returns a slice of StockAggregate pointers and an error if any occurred.
+func (d *DB) FetchLimitedTradeAfter(
+	ctx context.Context,
+	productID string,
+	timeFrame string,
+	start time.Time,
+	limit int) ([]*model.StockAggregate, error) {
+
+	reader := d.client.QueryAPI("org")
+	queryRes, err := reader.Query(ctx, fmt.Sprintf(
+		`from(bucket:"%s")
+			|> range(start:%d) 
+			|> filter(fn: (r) => r._measurement == "%s.%s")
+			|> filter(fn: (r) => (r._field == "open" or r._field == "close" or r._field == "high" or r._field == "low" or r._field == "volume"))
+			|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+			|> limit(n:%d)`,
+		d.tradeBucket, start.Unix(), productID, timeFrame, limit))
+	if err != nil {
+		return nil, err
+	}
+
+	// When query result is empty but error is not occurred
+	if queryRes == nil {
+		return nil, nil
+	}
+
+	data := []*model.StockAggregate{}
+	for queryRes.Next() {
+		aggregate := &model.StockAggregate{}
+
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "open", &aggregate.Open); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "open": %w`, err)
+		}
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "close", &aggregate.Close); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "close": %v\n`, err)
+		}
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "high", &aggregate.High); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "high": %v\n`, err)
+		}
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "low", &aggregate.Low); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "low": %v\n`, err)
+		}
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "volume", &aggregate.Volume); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "volume": %v\n`, err)
+		}
+		if err := extractFieldValueByKey(queryRes.Record().Values(), "_time", &aggregate.Time); err != nil {
+			return nil, fmt.Errorf(`extracting field: can't extract "_time": %v\n`, err)
+		}
+
+		data = append(data, aggregate)
+	}
+
+	return data, nil
+
+}
+
 func extractFieldValueByKey(values map[string]interface{}, key string, target any) error {
 
 	targetValue := reflect.ValueOf(target)

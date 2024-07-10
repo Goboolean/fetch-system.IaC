5138142a2b6a185fd4abe7dea7d1c1863ac77eb2
lsjtop10 <lsjtop10@gmail.com>
2024-07-10 11:37:34 +0900

    Refactor: Separate each test case into its own test function.

--- pkg/influx/queries_test.go
+++ pkg/influx/queries_test.go
@@ -51,127 +51,122 @@ func (suite *InfluxTestSuite) TestConstructor() {
 
 }
 
-func (suite *InfluxTestSuite) SetupSubTest() {
+func (suite *InfluxTestSuite) SetupTest() {
 	err := suite.recreateBucket(suite.options.Org, suite.options.TradeBucketName)
 	if err != nil {
 		suite.FailNow(err.Error())
 	}
 }
 
-func (suite *InfluxTestSuite) TestFetchByTimeRange() {
-	suite.Run("빈 bucket에 쿼리를 요청한 경우, data가 비어있어야 하고 에러가 없어야 한다.", func() {
-		// arrange
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
-		now := time.Now()
-		defer cancel()
+func (suite *InfluxTestSuite) TestFetchByTimeRange_ShouldReturnEmptyTradeWithoutError_WhenBucketIsEmpty() {
+	// arrange
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	now := time.Now()
+	defer cancel()
 
-		aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, "1m", now.Add(-5*60*time.Second), now)
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 0)
-	})
-
-	suite.Run("from과 to가 저장된 데이터의 범위 바깥을 나타내는 경우, data가 비어있어야 하고 에러가 없어야 한다", func() {
-		// arrange
-
-		start := time.Now()
-		suite.storeStockAggregate(start, time.Second, 60)
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
-		defer cancel()
-
-		aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, testTimeFrame, start.Add(-5*time.Second), start.Add(-1*time.Second))
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 0)
-	})
-
-	suite.Run("from과 to가 저장된 데이터를 포함하는 경우, 알맞은 개수의 데이터가 있어야 하고 에러가 없어야 한다", func() {
-		// arrange
-		start := time.Now()
-		suite.storeStockAggregate(start, time.Second, 60)
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
-		defer cancel()
-
-		aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, testTimeFrame, start, start.Add(30*time.Second))
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 30)
-	})
-
-	suite.Run("필수 필드가 빠졌을 때, 에러가 발생해야 한다.", func() {
-		// arrange
-		start := time.Now()
-		suite.storeBrokenStockAggregate(start, time.Second, 60)
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
-		defer cancel()
-
-		aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, "1m", start, start.Add(30*time.Second))
-		assert.Error(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 0)
-	})
+	aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, "1m", now.Add(-5*60*time.Second), now)
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 0)
 }
 
-func (suite *InfluxTestSuite) TestFetchLimitedTradeAfter() {
-	suite.Run("빈 bucket에 쿼리를 요청한 경우, data가 비어있어야 하고 에러가 없어야 한다.", func() {
-		// arrange
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
-		now := time.Now()
-		defer cancel()
+func (suite *InfluxTestSuite) TestFetchByTimeRange_ShouldReturnEmptyTrade_WhenFromAndToRepresentOutOfStoredRange() {
+	// arrange
 
-		aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, "1m", now.Add(-5*60*time.Second), 10)
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 0)
-	})
+	start := time.Now()
+	suite.storeStockAggregate(start, time.Second, 60)
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancel()
 
-	suite.Run("가져오려는 데이터보다 더 많은 데이터가 저장됐을 때, 원하는 만큼 데이터를 가져와야 한다.", func() {
-		// arrange
-		storeNum := 60
-		storeInterval := time.Second
-		start := time.Now().Add(time.Duration(-storeNum) * storeInterval)
-		suite.storeStockAggregate(start, storeInterval, storeNum)
+	aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, testTimeFrame, start.Add(-5*time.Second), start.Add(-1*time.Second))
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 0)
+}
 
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
-		defer cancel()
+func (suite *InfluxTestSuite) TestFetchByTimeRange_ShouldFetchData_WhenFromAndToIncludeRangeOfStoredData() {
+	// arrange
+	start := time.Now()
+	suite.storeStockAggregate(start, time.Second, 60)
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	defer cancel()
 
-		aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, testTimeFrame, start, 30)
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 30)
-	})
+	aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, testTimeFrame, start, start.Add(30*time.Second))
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 30)
+}
+func (suite *InfluxTestSuite) TestFetchByTimeRange_ShouldReturnError_WhenRequiredFieldNotExists() {
+	// arrange
+	start := time.Now()
+	suite.storeBrokenStockAggregate(start, time.Second, 60)
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	defer cancel()
 
-	suite.Run("가져오려는 데이터보다 더 적은 데이터가 저장됐을 때, 저장된 만큼 데이터를 가져와야 한다.", func() {
-		// arrange
-		storeNum := 20
-		storeInterval := time.Second
-		start := time.Now().Add(time.Duration(-storeNum) * storeInterval)
-		suite.storeStockAggregate(start, storeInterval, storeNum)
+	aggregates, err := suite.testClient.FetchByTimeRange(ctx, testStockID, "1m", start, start.Add(30*time.Second))
+	assert.Error(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 0)
+}
 
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
-		defer cancel()
+func (suite *InfluxTestSuite) TestFetchLimitedTradeAfter_ShouldReturnEmptyTradeWithoutError_WhenBucketIsEmpty() {
+	// arrange
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	now := time.Now()
+	defer cancel()
 
-		aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, testTimeFrame, start, 30)
-		assert.NoError(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 20)
-	})
+	aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, "1m", now.Add(-5*60*time.Second), 10)
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 0)
+}
 
-	suite.Run("필수 필드가 빠졌을 때, 에러가 발생해야 한다.", func() {
-		// arrange
-		storeNum := 60
-		storeInterval := time.Second
-		start := time.Now().Add(-time.Duration(storeNum) * storeInterval)
-		suite.storeBrokenStockAggregate(start, storeInterval, storeNum)
+func (suite *InfluxTestSuite) TestFetchLimitedTradeAfter_ShouldFetchLimitedAmountOfData_WhenMoreDataIsStored() {
+	// arrange
+	storeNum := 60
+	storeInterval := time.Second
+	start := time.Now().Add(time.Duration(-storeNum) * storeInterval)
+	suite.storeStockAggregate(start, storeInterval, storeNum)
 
-		// act
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
-		defer cancel()
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	defer cancel()
 
-		aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, "1m", start, 30)
-		assert.Error(suite.T(), err)
-		assert.Len(suite.T(), aggregates, 0)
-	})
+	aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, testTimeFrame, start, 30)
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 30)
+}
+
+func (suite *InfluxTestSuite) ShouldFetchAllData_WhenRequestedAmountExceedsStoredData() {
+	// arrange
+	storeNum := 20
+	storeInterval := time.Second
+	start := time.Now().Add(time.Duration(-storeNum) * storeInterval)
+	suite.storeStockAggregate(start, storeInterval, storeNum)
+
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	defer cancel()
+
+	aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, testTimeFrame, start, 30)
+	assert.NoError(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 20)
+}
+
+func (suite *InfluxTestSuite) ShouldFetchAllData_ShouldReturnError_WhenRequiredFieldNotExists() {
+	// arrange
+	storeNum := 60
+	storeInterval := time.Second
+	start := time.Now().Add(-time.Duration(storeNum) * storeInterval)
+	suite.storeBrokenStockAggregate(start, storeInterval, storeNum)
+
+	// act
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	defer cancel()
+
+	aggregates, err := suite.testClient.FetchLimitedTradeAfter(ctx, testStockID, "1m", start, 30)
+	assert.Error(suite.T(), err)
+	assert.Len(suite.T(), aggregates, 0)
 }
 
 func (suite *InfluxTestSuite) TearDownSuite() {
